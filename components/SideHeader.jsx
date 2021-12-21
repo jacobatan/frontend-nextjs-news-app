932dec756065a36e7413f070732d0c36e856f900
Jacob Tan <jacob.tan229@gmail.com>
2021-12-22 12:40:30 +1300

    changed the routing when the btn is clicked

--- components/SideHeader.jsx
+++ components/SideHeader.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const SideHeader = ({ topics }) => {
+  const router = useRouter();
   return (
     <div className="flex flex-col z-50 max-w-full">
       <p className="text-3xl font-bold sticky pb-3">topics</p>
 
       <div className="flex text-center sticky max-w-full flex-wrap">
         {topics.map((topic) => (
-          <button className="sideBtn">{topic}</button>
+          <button
+            className="sideBtn"
+            onClick={() => router.push(`/topics/${topic}`)}
+          >
+            {topic}
+          </button>
         ))}
       </div>
     </div>
