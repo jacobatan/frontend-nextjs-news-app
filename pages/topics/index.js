0e9149e3b526359302d5718ab002f8a6b867c287
Jacob Tan <jacob.tan229@gmail.com>
2021-12-23 00:18:52 +1300

    Removed unnecessary lines of code

--- pages/topics/index.js
+++ pages/topics/index.js
@@ -1,35 +1,12 @@
 import React from "react";
 import Link from "next/link";
 
-const PostList = ({ posts }) => {
+const PostList = () => {
   return (
     <>
-      <h1>List of Posts</h1>
-      {posts.map((post) => {
-        return (
-          <div key={post.id}>
-            <Link href={`post/${post.id}`} passHref>
-              <h2>
-                {post.id} {post.title}
-              </h2>
-            </Link>
-            <hr />
-          </div>
-        );
-      })}
+      <h1>johannah</h1>
     </>
   );
 };
-
-export async function getStaticProps() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
-
-  return {
-    props: {
-      posts: data.slice(0, 3),
-    },
-  };
-}
 
 export default PostList;
