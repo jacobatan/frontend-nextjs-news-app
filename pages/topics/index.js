46c3a7f2d983fc9771aaf45d1c7c7dd8941d8aad
Jacob Tan <jacob.tan229@gmail.com>
2021-12-24 12:21:30 +1300

    Removed unnecessary lines of code

--- pages/topics/index.js
+++ pages/topics/index.js
@@ -1,12 +1,1 @@
-import React from "react";
-import Link from "next/link";
-
-const PostList = () => {
-  return (
-    <>
-      <h1>johannah</h1>
-    </>
-  );
-};
-
-export default PostList;
+export default PostList = () => <h1>johannah</h1>;
